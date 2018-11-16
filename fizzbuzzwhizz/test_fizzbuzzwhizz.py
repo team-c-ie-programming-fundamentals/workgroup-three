5abd18bf764a38d956a4510d4d33c1506572b5f0
Anastasia Lasunina <anastasiala@student.ie.edu>
2018-11-16 14:16:51 +0100

    Test fizzbuzzwhiz all passed

--- fizzbuzzwhizz/test_fizzbuzzwhizz.py
+++ fizzbuzzwhizz/test_fizzbuzzwhizz.py
@@ -1,2 +1,40 @@
 # -*- coding: utf-8 -*-
 
+#%%
+from fizzbuzzwhizz import fizzbuzzwhiz
+
+
+def test_fizzbuzzwhiz_returns_fizz():
+    values = [ 6, 2358, 9]  
+    for case in values :
+        assert fizzbuzzwhiz(case) == "Fizz"
+
+def test_fizzbuzzwhiz_returns_buzz():
+    values = [10,1000, 50]
+    for case in values:
+        assert fizzbuzzwhiz(case) == "Buzz"
+        
+def test_fizzbuzzwhiz_returns_fizzbuzz():
+    values = [15,30]
+    for case in values:
+        assert fizzbuzzwhiz(case) == "FizzBuzz"
+
+def test_fizzbuzzwhiz_returns_num():
+    values = [8,16]
+    for case in values:
+        assert fizzbuzzwhiz(case) == case
+        
+def test_fizzbuzzwhiz_returns_undefined():
+    values = [0]
+    for case in values:
+        assert fizzbuzzwhiz(case) == "undefined"   
+
+def test_fizzbuzzwhiz_returns_whiz():
+    values = [2,11]
+    for case in values:
+        assert fizzbuzzwhiz(case) == "Whiz"     
+        
+def test_fizzbuzzwhiz_returns_notvalid():
+    values = [-11,-8]
+    for case in values:
+        assert fizzbuzzwhiz(case) == "Not valid" 
