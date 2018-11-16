b2c53facaa3eb25a13706005230a528ab114f2c3
Anastasia Lasunina <anastasiala@student.ie.edu>
2018-11-16 14:13:31 +0100

    Fizzbuzzwhiz completed

--- fizzbuzzwhizz/fizzbuzzwhizz.py
+++ fizzbuzzwhizz/fizzbuzzwhizz.py
@@ -1,2 +1,21 @@
-# -*- coding: utf-8 -*-
+#%%
+def fizzbuzzwhiz(num):
+    if num == 0:
+        return "undefined"
+    elif num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0 and num != 3:
+        return "Fizz"
+    elif num % 5 == 0 and num != 5:
+        return "Buzz"
+    elif num >=1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                return num
+        else:
+                return "Whiz"
+                
+    else:
+                return "Not valid"
+       
 
