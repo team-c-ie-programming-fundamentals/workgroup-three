45c14cdcbeef1789534ed92dca9962254e5c20ce
Lukas Hauser <lukas.raphael.hauser@gmail.com>
2018-11-19 15:20:11 +0100

    Update rock_paper_scissors/test_rock_paper_scissors.py
    Added tests for rpsls function

--- rock_paper_scissors/test_rock_paper_scissors.py
+++ rock_paper_scissors/test_rock_paper_scissors.py
@@ -8,4 +8,14 @@ def test_rockpaperscissors():
     assert rockpaperscissors('rock', 'scissors') == "Player 1 won!"
     assert rockpaperscissors('scissors', 'scissors') == "Play again!"
     assert rockpaperscissors('paper','scissors') == "Player 2 won!"
+
+from rock_paper_scissors import rpsls 
+
+def test_rpsls():
     
+    assert rpsls('rock', 'scissors') == "Player 1 won!"
+    assert rpsls('scissors', 'scissors') == "Play again!"
+    assert rpsls('paper','scissors') == "Player 2 won!"
+    assert rpsls('spock', 'scissors') == "Player 1 won!"
+    assert rpsls('lizzard', 'lizzard') == "Play again!"
+    assert rpsls('lizzard','spock') == "Player 1 won!"
